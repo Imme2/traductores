3168bd9e0ffb1da0e212432386503fbd119b77ca
Erick <ericksilvarodriguez@gmail.com>
2016-01-19 17:51:46 -0430

    Algunos cambios a Lexbot.c para limpiarlo y quitar variables globales.

--- Lexbot.c
+++ Lexbot.c
@@ -19,9 +19,35 @@ extern char* yytext;		// Se usa extern para traer desde lex.yy.c estos valores
 extern FILE* yyin;			// y la funcion yylex
 extern int yyleng;
 
-vector<token> tokens;		// Se declaran las listas de tokens y errores como globales para evitar
-vector<errToken> errores;	// retornar un par de vectores y dado que todas las funciones.
-							// usan estas variables
+
+// Firma de las funciones
+bool tokenizeFile(char* , vector<token>& , vector<errToken>& );
+void printTokens(vector<token>& , vector<errToken>&);
+
+
+// Esta es la funcion principal, aqui se chequean el numero de
+// argumentos y se imprimen por la salida estandar los errores
+// en caso de que el archivo no se pueda leer o no se le hayan dado
+// suficientes argumentos.
+
+int main(int argc,char* argv[]){
+	if (argc != 2){
+		printf("Numero de argumentos incorrecto, se esperaba 1 se recibieron %i\n",argc-1);
+		return 0;
+	}
+	vector<token> tokens;     		// Se definen las listas de errores y tokens respectivamente
+	vector<errToken> errores;		// para ser usadas en las funciones.
+
+	bool listo;
+	listo = tokenizeFile(argv[1],tokens,errores);
+	if (listo){
+		printTokens(tokens,errores);
+	}
+	else{
+		printf("El archivo %s no parece existir o no se puede leer\n",argv[1]);
+	}
+	return 0;
+}
 
 // Esta funcion recibe el nombre del archivo a tokenizar y 
 // llama al lexer para encontrar cada token.
@@ -29,7 +55,7 @@ vector<errToken> errores;	// retornar un par de vectores y dado que todas las fu
 // o de ser un error, a la lista de errores
 
 
-bool tokenizeFile(char* nombreArchivo){
+bool tokenizeFile(char* nombreArchivo, vector<token>& tokens, vector<errToken>& errores){
 	if ((yyin = fopen(nombreArchivo,"r")) != NULL){ //Se modifica el sitio de lectura
 		int tipo;									//si el archivo es invalido, esto
 		bool comment = false;						//retornara null, por lo que probamos
@@ -123,43 +149,20 @@ bool tokenizeFile(char* nombreArchivo){
 // en caso de que la lista de errores tenga elementos
 // en ella.
 
-void printTokens(){
+void printTokens(vector<token>& tokens, vector<errToken>& errores){
 	if (errores.size() != 0){
-		for (int i = 0; i < errores.size(); i++){
+		for (int i = 0; i < (int)errores.size(); i++){
 			cout << errores[i].toString() << endl;
 		}
 	}
 	else{
-		cout << tokens[0].toString();
-		for (int i = 1; i < tokens.size(); i++){
-			cout << ", " <<tokens[i].toString();
+		if (tokens.size() != 0){
+			cout << tokens[0].toString();
+			for (int i = 1; i < (int)tokens.size(); i++){
+				cout << ", " <<tokens[i].toString();
+			}
+			cout << endl;
 		}
-		cout << endl;
 	}
 
 }
-
-
-// Esta es la funcion principal, aqui se chequean el numero de
-// argumentos y se imprimen por la salida estandar los errores
-// en caso de que el archivo no se pueda leer o no se le hayan dado
-// suficientes argumentos.
-
-int main(int argc,char* argv[]){
-	if (argc != 2){
-		printf("Numero de argumentos incorrecto, se esperaba 1 se recibieron %i\n",argc-1);
-		return 0;
-	}
-	else{
-		bool listo;
-		listo = tokenizeFile(argv[1]);
-		if (listo){
-			printTokens();
-		}
-		else{
-			printf("El archivo %s no parece existir o no se puede leer\n",argv[1]);
-		}
-	}
-	return 0;
-}
-
